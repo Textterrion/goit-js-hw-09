a904feccfcd76bd0b6194ed26a0a78c091dd9e24
Textterrion <terry994566@gmail.com>
2026-02-28 22:45:02 +0200

    Refactor input event listener to trim whitespace from form data

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,20 +4,20 @@ const formData = { email: '', message: '' };
 
 const form = document.querySelector('.feedback-form');
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
-
-form.addEventListener('input', event => {
-  const { name, value } = event.target;
-  formData[name] = value.trim();
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
-});
-
 const savedData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
 if (savedData) {
   formData.email = savedData.email || '';
   formData.message = savedData.message || '';
   form.email.value = formData.email;
   form.message.value = formData.message;
 }
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+});
 
 form.addEventListener('submit', event => {
   event.preventDefault();
